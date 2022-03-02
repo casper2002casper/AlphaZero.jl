b85aec16d497070d5522d7816b4e70235d80ad1b
Casper <casper2002casper@gmail.com>
2022-03-02 12:51:40 +0100

    updated params and manifest

--- games/jsp2/params.jl
+++ games/jsp2/params.jl
@@ -4,31 +4,31 @@ netparams = NetLib.GinHP()
 
 self_play = SelfPlayParams(
   sim=SimParams(
-    num_games=32,
-    num_workers=8,
-    batch_size=4,
-    use_gpu=true,
+    num_games=64,
+    num_workers=24,
+    batch_size=12,
+    use_gpu=false,
     reset_every=1,
     flip_probability=0.,
     alternate_colors=false),
   mcts=MctsParams(
-    num_iters_per_turn=400,
+    num_iters_per_turn=5,
     cpuct=1.4,
     adaptive_cpuct = false,
-    temperature=ConstSchedule(0.),
-    dirichlet_noise_ϵ=0.,
-    dirichlet_noise_α=1.))
+    temperature=ConstSchedule(1.),
+    dirichlet_noise_ϵ=0.25,
+    dirichlet_noise_α=10/7))
 
 
 learning = LearningParams(
-  use_gpu=true,
+  use_gpu=false,
   use_position_averaging=false,
   samples_weighing_policy=CONSTANT_WEIGHT,
   l2_regularization=1e-4,
   optimiser=Adam(lr=5e-3),
-  batch_size=64,
-  loss_computation_batch_size=64,
-  nonvalidity_penalty=0.1,
+  batch_size=812,
+  loss_computation_batch_size=2048,
+  nonvalidity_penalty=0.,
   min_checkpoints_per_epoch=1,
   max_batches_per_checkpoint=2_000,
   num_checkpoints=1,
@@ -43,22 +43,22 @@ params = Params(
   memory_analysis=nothing,
   ternary_rewards=false,
   use_symmetries=false,
-  mem_buffer_size=PLSchedule(2_500))
+  mem_buffer_size=PLSchedule(10_000))
 
 benchmark_sim = SimParams(
   self_play.sim;
   num_games=12,
-  num_workers=4,
-  batch_size=2)
+  num_workers=12,
+  batch_size=6,
   deterministic = true)
 
-  benchmark = [
-    Benchmark.Single(
-      Benchmark.Full(self_play.mcts),
-      benchmark_sim),
-    Benchmark.Single(
-      Benchmark.NetworkOnly(),
-      benchmark_sim)]
+benchmark = [
+  Benchmark.Single(
+    Benchmark.Full(MctsParams(self_play.mcts, temperature=ConstSchedule(0.))),
+    benchmark_sim),
+  Benchmark.Single(
+    Benchmark.NetworkOnly(),
+    benchmark_sim)]
 
 experiment = Experiment(
   "jsp", GameSpec(), params, Network, netparams, benchmark)
