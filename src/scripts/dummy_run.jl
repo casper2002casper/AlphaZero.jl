796d8d9ddb2d6ca12f4e55035ee684fd137be050
Casper <casper2002casper@gmail.com>
2022-05-06 11:45:18 +0200

    disable gpu for dummy run

--- src/scripts/dummy_run.jl
+++ src/scripts/dummy_run.jl
@@ -12,9 +12,10 @@ dummy_run_player(p::Benchmark.Full) = @set p.params = dummy_run_mcts(p.params)
 dummy_run_player(p::Benchmark.MctsRollouts) = @set p.params = dummy_run_mcts(p.params)
 
 function dummy_run_sim(s::SimParams)
-  s = @set s.num_games = 2
+  s = @set s.num_games = 1
   s = @set s.num_workers = 1
   s = @set s.batch_size = 1
+  s = @set s.use_gpu = false
   return s
 end
 
