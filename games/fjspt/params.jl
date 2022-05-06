46f62fb60bd9f0c8bd711bcd75e42e1b1133b4b6
Casper <casper2002casper@gmail.com>
2022-05-06 14:11:13 +0200

    Init fjspt

---
+++ games/fjspt/params.jl
@@ -0,0 +1,66 @@
+Network = NetLib.Gin
+
+netparams = NetLib.GinHP()
+
+self_play = SelfPlayParams(
+  sim=SimParams(
+    num_games=256,
+    num_workers=200,
+    batch_size=100,
+    use_gpu=true,
+    reset_every=1,
+    flip_probability=0.,
+    alternate_colors=false),
+  mcts=MctsParams(
+    num_iters_per_turn=300,
+    cpuct=0.8,
+    adaptive_cpuct = false,
+    temperature=ConstSchedule(0.05),
+    dirichlet_noise_ϵ=0.,
+    dirichlet_noise_α=0.2))
+
+
+learning = LearningParams(
+  use_gpu=true,
+  use_position_averaging=false,
+  samples_weighing_policy=LOG_WEIGHT,
+  l2_regularization=1e-5,
+  optimiser=Adam(lr=1e-4),
+  batch_size=2048,
+  loss_computation_batch_size=2048,
+  nonvalidity_penalty=0.,
+  min_checkpoints_per_epoch=1,
+  max_batches_per_checkpoint=2_000,
+  num_checkpoints=1,
+  rewards_renormalization = 1)
+
+params = Params(
+  arena=nothing,
+  self_play=self_play,
+  learning=learning,
+  num_iters=50,
+  memory_analysis=nothing,
+  ternary_rewards=false,
+  use_symmetries=false,
+  mem_buffer_size=PLSchedule(500_000))
+
+benchmark_sim = SimParams(
+  self_play.sim;
+  num_games=64,
+  num_workers=44,
+  batch_size=22,
+  deterministic = true)
+
+benchmark = [
+  Benchmark.Single(
+    Benchmark.Full(MctsParams(self_play.mcts, temperature=ConstSchedule(0.))),
+    benchmark_sim),
+  Benchmark.Single(
+    Benchmark.NetworkOnly(),
+    benchmark_sim)]
+
+experiment = Experiment(
+  "fjspt", GameSpec(PLSchedule([1, 10], [2, 2])=>PLSchedule([1, 10], [2, 2]), 
+                  PLSchedule([1, 10], [2, 2])=>PLSchedule([1, 10], [2, 2]), 
+                  PLSchedule([1, 10], [2, 2])=>PLSchedule([1, 10], [2, 2]), 
+                  1=>5), params, Network, netparams, benchmark)
--- src/examples.jl
+++ src/examples.jl
@@ -20,13 +20,17 @@ module Examples
   include("../games/fjsp/main.jl")
   export FJSP
 
+  include("../games/fjspt/main.jl")
+  export FJSPT
+
   const games = Dict(
     "grid-world" => GridWorld.Training.experiment.gspec,
     "tictactoe" => Tictactoe.Training.experiment.gspec,
     "connect-four" => ConnectFour.Training.experiment.gspec,
     "mancala" => Mancala.Training.experiment.gspec,
     "jsp" => JSP.Training.experiment.gspec,
-    "fjsp" => FJSP.Training.experiment.gspec)
+    "fjsp" => FJSP.Training.experiment.gspec,
+    "fjspt" => FJSPT.Training.expiment.gspec)
     # "ospiel_ttt" => OSpielTictactoe.GameSpec()
   # ospiel_ttt is added from openspiel_example.jl when OpenSpiel.jl is imported
 
@@ -37,7 +41,8 @@ module Examples
     "connect-four" => ConnectFour.Training.experiment,
     "mancala" => Mancala.Training.experiment,
     "jsp" => JSP.Training.experiment,
-    "fjsp" => FJSP.Training.experiment)
+    "fjsp" => FJSP.Training.experiment,
+    "fjspt" => FJSPT.Training.experiment)
     # "ospiel_ttt" => OSpielTictactoe.Training.experiment
 
 end
