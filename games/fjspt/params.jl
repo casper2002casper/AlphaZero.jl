921e7655e11af9ef5bfbbebfbacb87ae44ba370d
Casper <casper2002casper@gmail.com>
2022-06-01 15:35:58 +0200

    update params

--- games/fjspt/params.jl
+++ games/fjspt/params.jl
@@ -4,15 +4,15 @@ netparams = NetLib.GinHP()
 
 self_play = SelfPlayParams(
   sim=SimParams(
-    num_games=PLSchedule([1, 10], [1200, 360]),
-    num_workers=PLSchedule([1, 10], [500, 300]),
+    num_games=PLSchedule([1, 10], [1500, 400]),
+    num_workers=PLSchedule([1, 10], [600, 275]),
     batch_size=PLSchedule([1, 10], [250, 150]),
     use_gpu=true,
     reset_every=1,
     flip_probability=0.,
     alternate_colors=false),
   mcts=MctsParams(
-    num_iters_per_turn=500,
+    num_iters_per_turn=400,
     cpuct=0.8,
     adaptive_cpuct = false,
     temperature=ConstSchedule(0.05),
@@ -23,10 +23,10 @@ self_play = SelfPlayParams(
 learning = LearningParams(
   use_gpu=true,
   use_position_averaging=false,
-  samples_weighing_policy=LOG_WEIGHT,
+  samples_weighing_policy=CONSTANT_WEIGHT,
   l2_regularization=1e-5,
-  optimiser=Adam(lr=1e-4),
-  batch_size=1300,
+  optimiser=Adam(lr=3e-4),
+  batch_size=1500,
   loss_computation_batch_size=2048,
   nonvalidity_penalty=0.,
   min_checkpoints_per_epoch=1,
@@ -38,11 +38,11 @@ params = Params(
   arena=nothing,
   self_play=self_play,
   learning=learning,
-  num_iters=50,
+  num_iters=500,
   memory_analysis=nothing,
   ternary_rewards=false,
   use_symmetries=false,
-  mem_buffer_size=PLSchedule(500_000))
+  mem_buffer_size=PLSchedule(300_000))
 
 benchmark_sim = SimParams(
   self_play.sim;
