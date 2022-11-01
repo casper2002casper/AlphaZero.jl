2161ced9dd63acb505e5b45dc3b90d2ddeb23dcc
Casper <casper2002casper@gmail.com>
2022-11-01 12:11:52 +0100

    use dummyrun for data gen

--- src/scripts/dummy_run.jl
+++ src/scripts/dummy_run.jl
@@ -5,16 +5,16 @@
 
 using Setfield
 
-dummy_run_mcts(p::MctsParams) = @set p.num_iters_per_turn = 4
+dummy_run_mcts(p::MctsParams) = @set p.num_iters_per_turn = 2
 
 dummy_run_player(p::Benchmark.Player) = p
 dummy_run_player(p::Benchmark.Full) = @set p.params = dummy_run_mcts(p.params)
 dummy_run_player(p::Benchmark.MctsRollouts) = @set p.params = dummy_run_mcts(p.params)
 
 function dummy_run_sim(s::SimParams)
-  s = @set s.num_games = ConstSchedule(1)
-  s = @set s.num_workers = ConstSchedule(1)
-  s = @set s.batch_size = ConstSchedule(1)
+  s = @set s.num_games = ConstSchedule(1000)
+  s = @set s.num_workers = ConstSchedule(20)
+  s = @set s.batch_size = ConstSchedule(10)
   s = @set s.use_gpu = false
   return s
 end
