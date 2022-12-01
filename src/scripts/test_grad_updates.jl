513e33a7a4c0b46a67284ffb74b6717a1bacac59
Casper <casper2002casper@gmail.com>
2022-12-01 20:53:24 +0100

    add test script

--- src/scripts/test_grad_updates.jl
+++ src/scripts/test_grad_updates.jl
@@ -1,3 +1,4 @@
+using Setfield
 """
     test_grad_updates(experiment; [num_games])
 
@@ -6,8 +7,12 @@ Run a gradient update phase using dummy data.
 This is useful to ensure that the chosen hyperparameters do not
 lead to _Out of Memory_ errors during training.
 """
-function test_grad_updates(exp::Experiment; num_games=5000)
-  session = Session(exp, autosave=false, dir="sessions/grad-updates-$(exp.name)")
+function test_grad_updates(exp::Experiment; num_games=200, batch_size = exp.params.learning.batch_size, loss_batch_size = exp.params.learning.loss_computation_batch_size)
+  exp = @set exp.params.learning.batch_size = batch_size
+  exp = @set exp.params.learning.loss_computation_batch_size = loss_batch_size 
+  dir="sessions/grad-updates-$(exp.name)"
+  rm(dir, force=true, recursive=true)
+  session = Session(exp, autosave=false, dir=dir)
   UI.Log.section(session.logger, 1, "Generating $num_games some random traces")
   for i in 1:num_games
     trace = play_game(exp.gspec, RandomPlayer())
