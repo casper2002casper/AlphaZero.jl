05fdb8761bb478c8279ecd29f3b9034e9169a312
Casper <casper2002casper@gmail.com>
2022-12-22 16:41:34 +0100

    Revert "remove M & V for memory"
    This reverts commit ff221814ea2a3689a12b9891aea0d44f1996c532.

--- src/networks/flux.jl
+++ src/networks/flux.jl
@@ -161,9 +161,12 @@ function Network.forward(nn::GATGraphNeuralNetwork, g)
 
   A = adjacency_matrix(g, Bool, nodes = is_next_op)
 
-  index = vcat([fill(o, sum(A[o, is_machine]) * sum(A[o, is_vehicle])) for o in eachindex(next_op_nodes)]...)
-  machine_index = vcat([repeat(findall(A[o, is_machine]), inner=sum(A[o, is_vehicle])) for o in eachindex(next_op_nodes)]...)
-  vehicle_index = vcat([repeat(findall(A[o, is_vehicle]), outer=sum(A[o, is_machine])) for o in eachindex(next_op_nodes)]...)
+  M = A .& is_machine'
+  V = A .& is_vehicle'
+
+  index = vcat([fill(o, sum(M[o,:]) * sum(V[o,:])) for o in eachindex(next_op_nodes)]...)
+  machine_index = vcat([repeat(findall(M[o,:]), inner=sum(V[o,:])) for o in eachindex(next_op_nodes)]...)
+  vehicle_index = vcat([repeat(findall(V[o,:]), outer=sum(M[o,:])) for o in eachindex(next_op_nodes)]...)
   operation_index = next_op_nodes[index]
   graph_index = g.graph_indicator[operation_index]
   
