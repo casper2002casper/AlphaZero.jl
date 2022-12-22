86ce8c5205a7ce5ebfca041189d2f893f3df3d75
Casper Pennings <chpennings@login1.hpc.tudelft.nl>
2022-12-22 14:44:50 +0100

    update params

--- games/fjspt3/params.jl
+++ games/fjspt3/params.jl
@@ -1,10 +1,11 @@
+using Optimisers
 Network = NetLib.Gat
 
 netparams = NetLib.GatHP()
 
 self_play = SelfPlayParams(
   sim=SimParams(
-    num_games=PLSchedule([1, 10], [2000, 2000]),
+    num_games=PLSchedule([1, 10], [1000, 1000]),
     num_workers=PLSchedule([1, 10], [20, 20]),
     batch_size=PLSchedule([1, 10], [10, 10]),
     use_gpu=true,
@@ -12,21 +13,22 @@ self_play = SelfPlayParams(
     flip_probability=0.,
     alternate_colors=false),
   mcts=MctsParams(
-    num_iters_per_turn=600,
-    cpuct=1.3,
-    temperature=PLSchedule([1, 10], [1.0, 0.0]),
-    dirichlet_noise_ϵ=0.25,
-    dirichlet_noise_α=0.05))
+    num_iters_per_turn=500,
+    cpuct=1.4,
+    temperature=PLSchedule([1, 5], [1.0, 0.0]),
+    dirichlet_noise_ϵ=0.1,
+    dirichlet_noise_α=0.03))
 
 
 learning = LearningParams(
   use_gpu=true,
   use_position_averaging=false,
   samples_weighing_policy=CONSTANT_WEIGHT,
-  l2_regularization=1e-5,
-  optimiser=ScheduledAdam(lr=PLSchedule([1, 6, 10], [1e-3, 1e-4, 5e-5])),
-  batch_size=3250,
-  loss_computation_batch_size=1500,
+  l2_regularization=1e-4,
+  optimiser= Optimisers.Nesterov(1e-2, 0.9), 
+  learnrate = PLSchedule([1, 2, 10], [1e-2, 1e-3, 1e-4]), 
+  batch_size=1_000,
+  loss_computation_batch_size=1_500,
   nonvalidity_penalty=0.,
   min_checkpoints_per_epoch=1,
   max_batches_per_checkpoint=2_000,
@@ -36,8 +38,8 @@ learning = LearningParams(
 benchmark_sim = SimParams(
   self_play.sim;
   num_games=PLSchedule([1, 10], [44, 44]),
-  num_workers=ConstSchedule(31),
-  batch_size=ConstSchedule(16),
+  num_workers=ConstSchedule(20),
+  batch_size=ConstSchedule(10),
   deterministic = true)
 
 benchmark = [
@@ -48,7 +50,7 @@ benchmark = [
     Benchmark.NetworkOnly(),
     benchmark_sim)]
 
-  arena = ArenaParams(
+arena = ArenaParams(
   sim=benchmark_sim,
   mcts=MctsParams(self_play.mcts, temperature=ConstSchedule(0.),  dirichlet_noise_ϵ=0.),
   update_threshold=0.01)
@@ -61,7 +63,7 @@ params = Params(
   memory_analysis=nothing,
   ternary_rewards=false,
   use_symmetries=false,
-  mem_buffer_size=PLSchedule(1_000_000))
+  mem_buffer_size=PLSchedule(300_000))
 
 experiment = Experiment(
   "fjspt3", GameSpec(PLSchedule([1, 10], [2, 2])=>PLSchedule([1, 10], [7, 7]), 
